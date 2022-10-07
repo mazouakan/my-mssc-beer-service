601a73f347d36e1957b002bcb981999fb3846d06
mazoukan <azouakanmed@gmail.com>
2022-10-07 21:00:02 +0200

    initial commit add

---
+++ src/main/java/com/azouakan/mymsscbeerservice/MyMsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.azouakan.mymsscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMsscBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMsscBeerServiceApplication.class, args);
+	}
+
+}
